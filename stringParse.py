1882e8bd895d0ad7a26fde67e1f8978aa7ff9d0a
Astrid La Reine Lockhart <astrid.lr.lockhart@gmail.com>
2025-01-24 06:53:40 -0600

    Add files via upload

---
+++ stringParse.py
@@ -0,0 +1,7 @@
+dataVar = 'abcdefghijklmnopqrstuvwxyz'
+
+
+for i in range(0, len(dataVar)):
+    print(f'{i} {dataVar[i]}') # Prints the index of each letter
+
+print(dataVar[3:7]) # printing "d"-"g"
