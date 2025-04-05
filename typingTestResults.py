6244cacd759e2695ca64170c3144f9c9ff407b2a
Astrid La Reine Lockhart <astrid.lr.lockhart@gmail.com>
2025-04-05 11:28:35 -0500

    Update typingTestResults.py

--- typingTestResults.py
+++ typingTestResults.py
@@ -6,8 +6,15 @@ urlHack = open('urlPath.txt', 'r')
 baseUrl = requests.get('https://api.monkeytype.com/results', headers=header) # to add params include [, params=mode ]
 responce = baseUrl.json()
 
+i = 0
+average_wpm = 0
+while i < 10:
+   average_wpm = average_wpm + responce['data'][i]['wpm']
+   i += 1
+
 print("User: " + str(responce['data'][0]['name']))
-print("Words Per Minute: " + str(responce['data'][0]['wpm'])) 
+print("Last test WPM: " + str(responce['data'][0]['wpm']))
+print("Average WPM: " + str(round(average_wpm / 10)))
 
 # This is for retriveing the most recent typing test results from MonkeyType (https://monkeytype.com/) using 
 # their API (https://api.monkeytype.com/docs#tag/results/operation/results.get)
