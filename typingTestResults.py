fc5704da4c8ab53399c71127368bcd88b4a9d42f
Astrid La Reine Lockhart <astrid.lr.lockhart@gmail.com>
2025-04-05 11:41:40 -0500

    Update typingTestResults.py

--- typingTestResults.py
+++ typingTestResults.py
@@ -8,13 +8,13 @@ responce = baseUrl.json()
 
 i = 0
 average_wpm = 0
-while i < 10:
+while i < len(responce['data']):
    average_wpm = average_wpm + responce['data'][i]['wpm']
    i += 1
 
 print("User: " + str(responce['data'][0]['name']))
 print("Last test WPM: " + str(responce['data'][0]['wpm']))
-print("Average WPM: " + str(round(average_wpm / 10)))
+print("Average WPM: " + str(round(average_wpm / i)))
 
 # This is for retriveing the most recent typing test results from MonkeyType (https://monkeytype.com/) using 
 # their API (https://api.monkeytype.com/docs#tag/results/operation/results.get)
